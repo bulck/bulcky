9ff3e1567de195981f8f2f1d3a23081d9187a876
ygene <yann.genevois@gmail.com>
2016-08-07 09:30:40 +0200

    Fixe

--- 02_bulcky_services/09_bulckyCam/appGoPro.sh
+++ 02_bulcky_services/09_bulckyCam/appGoPro.sh
@@ -24,27 +24,35 @@ fi
 
 if [ "$1" == "" ]; then
     usage
+    exit 3
 fi
 
 
 case "$1" in
     --list)  
-        if [ "$GOPRO_VERSION" == "SESSION" ];
+        if [ "$GOPRO_VERSION" == "SESSION" ]; then
             /usr/bin/wakeonlan -i 10.5.5.9 -p 9 $GOPRO_MAC
         fi 
-
         /usr/bin/wget -q -O -  http://10.5.5.9:8080/gp/gpMediaList        
     ;;
     --takePhoto)  
-        if [ "$GOPRO_VERSION" == "SESSION" ];
+        if [ "$GOPRO_VERSION" == "SESSION" ]; then
             /usr/bin/wakeonlan -i 10.5.5.9 -p 9 $GOPRO_MAC
         fi 
-        /usr/bin/wget http://10.5.5.9/gp/gpControl/command/mode?p=1 -O -
-        /usr/bin/wget http://10.5.5.9/gp/gpControl/command/shutter?p=1 -O -
+        /usr/bin/wget -q http://10.5.5.9/gp/gpControl/command/mode?p=1 -O - >/dev/null
+        if [ $? -eq 0 ]; then
+            /usr/bin/wget -q http://10.5.5.9/gp/gpControl/command/shutter?p=1 -O - >/dev/null
+            if [ $? -ne 0 ]; then
+                exit 5
+            else
+                echo "--> A new photo has been taken"
+            fi
+        else
+            exit 4
+        fi
     ;;
     *) usage
    ;;
 esac
 
 exit 0
-
